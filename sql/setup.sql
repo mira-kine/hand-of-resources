4f715dd19fd7130b2f6d3a7b0211809be168ea57
mira-kine <mirak0419@gmail.com>
2022-03-17 10:07:48 -0700

    songs model working

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS songs;
+
+CREATE TABLE songs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    artist TEXT NOT NULL
+);
