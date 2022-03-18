bcc843c879658a9a0106cab863040e019006a5c8
mira-kine <mirak0419@gmail.com>
2022-03-18 12:07:52 -0700

    wrote get all foods, controller and model next

--- __tests__/foods.test.js
+++ __tests__/foods.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Foods = require('../lib/models/Foods');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -12,12 +13,21 @@ describe('hand-of-resources routes', () => {
     pool.end();
   });
 
-  it.only('creates a food', async () => {
+  it('creates a food', async () => {
     const expected = {
       name: 'Katsu curry',
       cost: '500 yen',
     };
     const res = await request(app).post(`/api/v1/foods`).send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets all foods', async () => {
+    const expected = await Foods.createFood({
+      name: 'Katsu curry',
+      cost: '500 yen',
+    });
+    const res = await request(app).get(`/api/v1/foods`);
+    expect(res.body).toEqual(expected);
   });
 });
