679e89bd1b65b44307e17064287581e017cfc6c8
mira-kine <mirak0419@gmail.com>
2022-03-17 17:29:39 -0700

    wrote get all books test

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Books = require('../lib/models/Books');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -12,12 +13,25 @@ describe('hand-of-resources routes', () => {
     pool.end();
   });
 
-  it.only('should create book', async () => {
+  it('should create book', async () => {
     const expected = {
       name: 'Crying in H Mart',
       author: 'Michelle Zauner',
     };
     const res = await request(app).post('/api/v1/books').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it.only('gets all books', async () => {
+    const book1 = await Books.createBook({
+      name: 'Crying in H Mart',
+      author: 'Michelle Zauner',
+    });
+    const book2 = await Books.createBook({
+      name: 'Six of Crows',
+      author: 'Leigh Bardugo',
+    });
+    const res = await request(app).get('/api/v1/books');
+    expect(res.body).toEqual(expect.arrayContaining([book1, book2]));
   });
 });
